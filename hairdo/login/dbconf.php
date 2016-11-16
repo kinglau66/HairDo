f26c6315f10f694892f52caabb2785faca398d6c
wilsonting <wlsn_ting520@hotmail.com>
2016-11-16 14:03:12 +0800

    Add the structure of login and the display card layout

---
+++ hairdo/login/dbconf.php
@@ -0,0 +1,6 @@
+<?php
+    //DATABASE CONNECTION VARIABLES
+    $host = "localhost"; // Host name
+    $username = "user"; // Mysql username
+    $password = "password"; // Mysql password
+    $db_name = "login"; // Database name
